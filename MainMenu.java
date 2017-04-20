4e46885e09d7685144af26a85f87e50be9866a76
lisakoga <ls256469@dal.ca>
2017-04-20 16:33:20 -0300

    version 2

--- MainMenu.java
+++ MainMenu.java
@@ -1,46 +1,66 @@
 import java.util.Scanner;
 public class MainMenu 
 {
+	@SuppressWarnings("static-access")
 	public static void main(String [] args)
 	{
 		Scanner keyboard = new Scanner (System.in);
+		boolean play = true;
 		
-		System.out.println("****CRAZY EIGHTS****");
-		System.out.println("\nWhat would you like to do? Please enter:\nNew Game\nRules\nQuit");
-		String option = keyboard.nextLine();
-		
-		while (!option.equalsIgnoreCase("quit"))
+		while (play)
 		{
+			System.out.println("****CRAZY EIGHTS****");
+			System.out.println("\nWhat would you like to do? Please enter:\nNew Game\nRules\nQuit");
+			String option = keyboard.nextLine();
+		
+			// show rules
 			if (option.equalsIgnoreCase("rules"))
 			{
-				String rules = "You have five cards in your hand to begin, and you want to get rid of all of those cards."
-						+ "\nEach turn, the you must place a legal card onto the discard pile."
-						+ "\nIf you don't have a leagl card, you must draw cards from the stock until you pick up a card that can be played."
-						+ "\nIf the stock runs out, shuffle the discard pile and continue drawing cards."
-						+ "\nIf the top card on the discard pile is not an 8, you can play any card that matches the number or suit of the card."
-						+ "\nAll of the eights are the wild. If you put an 8 down, you must declare what suit you want to change it to."
-						+ "\nThe next player must either play a card of the declared suit or play another 8.";
+				String rules = "You have five cards in your hand to begin, and the goal is to get rid of all of them."
+						+ "\nEach turn, the you must play a card onto the discard pile. This card must match either the number or the suit of the face up card."
+						+ "\nIf you don't have any cards that you can play, you must draw cards from the deck until you pick up a card that can be played."
+						+ "\nIf the deck runs out, the discard pile will be shuffled and you may continue drawing cards."
+						+ "\nAll of the eights are wild; You can play an eight on top of any card and when you do, you must declare"
+						+ "\nwhich suit you would to change it to. If an eight is face up, you must either play a card of the declared suit or play another 8.";
 				System.out.println(rules);
 			}
+			// start a new game
 			else if (option.equalsIgnoreCase("new game"))
 			{
-				// ***I have still been working on this section***
+				crazyEights game = new crazyEights();
+				game.getDeck().putFirstCard();
+				game.getDeck().setTopUsed(game.getDeck().getOrigincards().get(0));
 				
-				//Game newGame = new Game();
-				crazyEights crazyEight = new crazyEights();
-				crazyEight.play();
+				while (!game.winner())
+				{
+					// a player's turn
+					game.play();
+					// ai's turn
+					System.out.println(game.ai.takeTurn());
+					
+					if (game.playerHand.isEmpty())
+					{
+						System.out.println("You win!");
+					}
+					if (game.compHand.isEmpty())
+					{
+						System.out.println("You lose...");
+					}
+				}
 			}
+			// quit the game
+			else if (option.equalsIgnoreCase("quit"))
+			{
+				System.out.println("Thank you for playing!");
+				play = false;
+				keyboard.close();
+				System.exit(0);
+			}
+			// if input was invalid
 			else
 			{
-				System.out.println("Invalid input");
+				System.out.println("Invalid input. Please try again.");
 			}
-			System.out.println("\nWhat would you like to do? Please enter:\nNew Game\nRules\nQuit");
-			option = keyboard.nextLine();
-		}
-		if (option.equalsIgnoreCase("quit"))
-		{
-			System.out.println("Thank you for playing!");
 		}
 	}
-	
 }
